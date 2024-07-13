a37070e6e5b44d5e939e6295f68703fe12b6b12d
gauri <dahakegauri@4gmail.com>
2024-07-13 21:56:42 +0530

    array js

---
+++ array task.js
@@ -0,0 +1,5 @@
+let ar=[10,8,7,5]
+let a=parseInt(prompt("eenter your number"))
+let b=parseInt(prompt("enter num"))
+ar.push(a,b)
+document.write(ar)
---
+++ function array.js
@@ -0,0 +1,7 @@
+let ar =[1,2,3,4,5]
+
+//console.log(ar.push(6,7,8,9))
+//console.log(ar.pop())
+//ar.unshift("khushi")
+ar.shift()
+document.write(ar)
