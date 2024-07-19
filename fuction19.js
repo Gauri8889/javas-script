ab7d10f035487aa0e744e176df04f470ba13f860
gauri <dahakegauri@4gmail.com>
2024-07-19 22:13:10 +0530

    function

---
+++ fuction19.js
@@ -0,0 +1,24 @@
+/*function demo (b)
+{
+    return b+b;
+}
+var a=10
+console.log(demo(a))*/
+
+
+/*function khushi(c)
+{
+    console.log(c)
+}
+var age=20
+khushi(age)*/
+
+
+
+//WAP to take input from the user print of the number using with argument and return value?
+function cube(n)
+{
+    return n*n*n;
+}
+let n=parseInt(prompt("enter cube value"))
+console.log(cube(n))
