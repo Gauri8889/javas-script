8fbd65c6d80f8aab35efd751c6269e86fd653b87
gauri <dahakegauri@4gmail.com>
2024-07-25 23:10:04 +0530

    Dom even

---
+++ Dom even.js
@@ -0,0 +1,15 @@
+let read = document.getElementById('read')
+//read.style.backgroundColor='pink'
+//read.style.padding="20"
+read.style.cssText = "background-color: red; color:white; padding:20px";
+
+//changing element and text
+read.innerHTML = "<u> ORACAL </u>"
+
+
+//changing only text
+read.innerText = "text chenge"
+
+//changing image
+let image =document.getElementById('image1')
+image.src ="gauri.jpg"
