646757de5811c8198db7fb880fd6d23dd6ee5cb0
gauri <dahakegauri@4gmail.com>
2024-08-06 23:01:59 +0530

    array destrcturing function

---
+++ array5agust.js
@@ -0,0 +1,28 @@
+// // map fuction
+// let ar = [1,2,3,4,5];
+// let p = ar.map((t)=>{return t*t})
+// for(let i=0; i<=5;i++)
+// {
+//     console.log(ar[i]*ar[i]);
+//     console.log(p)
+    
+    
+// }
+
+//fuction ke under fuction ko call by fuction kahte hai or heigher fuction bhi kahte hai
+
+//spreat oparetor
+
+// let ob = {emp_name:"khushi",
+//           emp_dest:"HR",
+// }
+// let ob1={emp_salary:100000,}
+// console.log(ob,ob1)
+
+//destructuring fuction
+
+let{empname,empage}={
+    emp_name :"khushi",
+    emp_dest:"HR"
+}
+console.log(empname)
